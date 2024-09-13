87303ca109c0fb0c2988adddb7773cdd83d67ef8
juanpa27 <jbellenzier@gmail.com>
2024-09-12 21:27:40 -0400

    Cambio en header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from "@/components/mode-toggle"
 const nombre = '${DolarPy}';
+
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 h-12 bg-gray-100 dark:bg-gray-900 flex items-center justify-between px-4 mb-6">
